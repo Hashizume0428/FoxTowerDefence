82152e31c9c2055bc2c73e031266d6b184da300a
Hashizume0428 <s2310214.hashizume.cgp@gmail.com>
2026-01-09 21:07:10 +0900

    プレイヤーコントローラーを書き始めてみた

---
+++ FoxTowerDefence/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Headdrer("Player Status Settings")]
+    [SerializeField]
+    private float hp = 10f; //プレイヤーのHP
+    [SerializeField]
+    private float attackPower = 1f; //プレイヤーの攻撃力
+    [SerializeField]
+    private float moveSpeed = 5f; //プレイヤーの移動速度
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
